f88474220775fd7195f90f0b6d671e26a5089ca6
Daniil Ostrerov <ostrerov.daniil@gmail.com>
2023-07-04 11:02:18 +0300

    HW5 - done 2

--- app/Console/Commands/UserCommand.php
+++ app/Console/Commands/UserCommand.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Storage;
+use Psy\Readline\Hoa\ConsoleException;
 
 class UserCommand extends Command
 {
@@ -15,49 +16,46 @@ class UserCommand extends Command
 
     public function handle()
     {
-        $confirm = true;
         $this->data['name'] = $this->ask('What is your name?');
         $this->data['age'] = $this->ask('How old are you?');
-        if ($this->data['age'] < 18) {
-            if ($this->confirm('This command 18+. Do you want to continue?') === false) {
-                $confirm = false;
-                $this->error('You chose \'No\'. Exit from command');
-            }
+        if ($this->data['age'] < 18 AND !$this->confirm('This command 18+. Do you want to continue?')) {
+            $this->error('You chose \'No\'. Exit from command');
+            return false;
         }
 
-        if ($confirm) {
-            $option = $this->choice('Choice your option', ['read', 'write']);
-            if ($option === 'read') {
-                if (Storage::disk('local')->exists('Commands/UserCommand.txt')) {
-                    $file = Storage::disk('local')->get('Commands/UserCommand.txt');
-                    if (!empty($file)) {
-                        $this->comment('Data: ' . $file);
-                    } else {
-                        $this->info('File is empty. End command');
-                    }
-                } else {
-                    $this->error('Error! File not found!');
-                }
+        $option = $this->choice('Choice your option', ['read', 'write']);
+        if ($option === 'read' AND Storage::disk('local')->exists('Commands/UserCommand.txt')) {
+            $file = Storage::disk('local')->get('Commands/UserCommand.txt');
+            if (empty($file)) {
+                $this->info('File is empty. End command');
+                return false;
             }
-
-            if ($option === 'write') {
-                $this->data['sex'] = $this->choice('What is your gender?', ['male', 'female', 'other']);
-                if ($this->data['sex'] === 'other') {
-                    $this->data['sex'] = $this->ask('What is your gender name?');
-                }
-                $this->data['phone'] = $this->ask('What is your phone number?');
-                $this->data['city'] = $this->ask('Where are you live?');
-
-                $data = json_encode($this->data, JSON_FORCE_OBJECT);
-
-                if (Storage::disk('local')->put('Commands/UserCommand.txt', $data)) {
-                    $file = Storage::disk('local')->get('Commands/UserCommand.txt');
-                    $this->info('Data wrote successful!');
-                    $this->comment('Data: ' . $file);
-                } else {
-                    $this->error('Writing error!');
-                }
-            }
+            $this->comment('Data: ' . $file);
+            return true;
         }
+
+        if ($option === 'write') {
+            $this->data['sex'] = $this->choice('What is your gender?', ['male', 'female', 'other']);
+            if ($this->data['sex'] === 'other') {
+                $this->data['sex'] = $this->ask('What is your gender name?');
+            }
+            $this->data['phone'] = $this->ask('What is your phone number?');
+            $this->data['city'] = $this->ask('Where are you live?');
+
+            $data = json_encode($this->data, JSON_FORCE_OBJECT);
+
+            if (Storage::disk('local')->put('Commands/UserCommand.txt', $data)) {
+                $file = Storage::disk('local')->get('Commands/UserCommand.txt');
+                $this->info('Data wrote successful!');
+                $this->comment('Data: ' . $file);
+                return true;
+            }
+
+            $this->error('Writing error!');
+            return false;
+        }
+
+        $this->error('Error! File not found!');
+        return false;
     }
 }
