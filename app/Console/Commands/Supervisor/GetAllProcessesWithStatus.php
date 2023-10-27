e49b9d687b7572199a5964ac33c098e663cc9e6f
Daniil Ostrerov <ostrerov.daniil@gmail.com>
2023-10-28 00:37:54 +0300

    HW22

---
+++ app/Console/Commands/Supervisor/GetAllProcessesWithStatus.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Console\Commands\Supervisor;
+
+use Illuminate\Console\Command;
+
+class GetAllProcessesWithStatus extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'supervisor:get-all';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Show all processes in supervisor with status';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $processes = [];
+        exec('supervisorctl status', $processes);
+
+        foreach ($processes as $process) {
+            echo $process . PHP_EOL;
+        }
+    }
+}
