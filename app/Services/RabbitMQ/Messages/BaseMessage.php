6c673512baf0503da85d86adf8ddc4e35eb5993a
Daniil Ostrerov <ostrerov.daniil@gmail.com>
2023-10-28 08:40:40 +0300

    HW24

---
+++ app/Services/RabbitMQ/Messages/BaseMessage.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Services\RabbitMQ\Messages;
+
+use Carbon\Carbon;
+use JsonSerializable;
+use ReflectionClass;
+
+class BaseMessage implements JsonSerializable
+{
+    public function __construct(object $data)
+    {
+        $reflect = new ReflectionClass($this);;
+
+        foreach ($reflect->getProperties() as $property) {
+            $propertyName = $property->getName();
+            $type = $property->getType()->getName();
+            $value = $data->$propertyName;
+
+            if (enum_exists($type) === true) {
+                $value = $type::from($value);
+            }
+
+            if ($type === Carbon::class) {
+                $value = Carbon::parse($value);
+            }
+
+            $this->$propertyName = $value;
+        }
+    }
+
+    /**
+     * Specify data which should be serialized to JSON
+     * @link https://php.net/manual/en/jsonserializable.jsonserialize.php
+     * @return mixed data which can be serialized by <b>json_encode</b>,
+     * which is a value of any type other than a resource.
+     * @since 5.4
+     */
+    public function jsonSerialize(): array
+    {
+        return get_object_vars($this);
+    }
+}
